281c3ac7273ed984bf7a9ff38430708f50112ad7
Tomas Garcia <tomasotano25@gmail.com>
2022-10-26 21:13:40 -0400

    usando token en cookie

---
+++ pages/dashboard.js
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import axios from "axios";
+
+export default function Dashboard() {
+  const [user, setUser] = useState({
+    email: "",
+    username: "",
+  });
+
+  const getProfile = async () => {
+    const response = await axios.get("/api/profile");
+    setUser(response.data);
+  };
+
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <pre>{JSON.stringify(user, null, 2)}</pre>
+      <button onClick={getProfile}>get profile</button>
+    </div>
+  );
+}
